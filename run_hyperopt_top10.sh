c006a8be9f1a412b1c48649f7de36e36952a91ae
tibicrypto <dmpakpahan025@outlook.com>
2026-02-16 08:42:03 +0000

    edittimerange

--- run_hyperopt_top10.sh
+++ run_hyperopt_top10.sh
@@ -6,7 +6,7 @@ source .venv/bin/activate
 # Configuration
 STRATEGY="NostalgiaForInfinityX7"
 CONFIG_PAIRLIST="configs/pairlist-hyperopt-static-binance-futures-usdt.json"
-TIMERANGE="20250101-20251213"
+TIMERANGE="20250101-20260201"
 TIMEFRAMES="5m 1h 15m 4h 1d"
 
 # Menu for user selection
